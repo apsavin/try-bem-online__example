b0a60ec832ade09e2810a90f49684bb368d4e844
Alexander Savin <box.for.postman@gmail.com>
2014-02-27 21:19:21 +0400

    cache for libs

--- .bem/make.js
+++ .bem/make.js
@@ -12,6 +12,13 @@ MAKE.decl('Arch', {
     blocksLevelsRegexp: /^.+?\.blocks/,
     bundlesLevelsRegexp: /^.+?\.bundles$/,
 
+    getLevelCachePolicy: function() {
+        return {
+            cache: false,
+            except: ['libs/bem-core', 'libs/bem-components']
+        }
+    },
+
     libraries: [
         'bem-core @ f4b46ef0590549042d938f7e981df4d14eb4caef',
         'bem-components @ 82301a8af6c15c2849d1f755a24f594de6522251'
