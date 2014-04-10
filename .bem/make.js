dbfdf8464d85a9434d9b4d79a43e3c337668000f
Alexander Savin <box.for.postman@gmail.com>
2014-04-10 23:04:24 +0400

    update from project-stub + concrete blocks levels in Arch

--- .bem/make.js
+++ .bem/make.js
@@ -1,35 +1,33 @@
 /* jshint node:true */
 /* global MAKE */
 
-var environ = require('bem-environ')(__dirname);
-environ.extendMake(MAKE);
-
-//process.env.YENV = 'production';
-//process.env.XJST_ASYNCIFY = 'yes';
-
 MAKE.decl('Arch', {
 
-    blocksLevelsRegexp: /^.+?\.blocks/,
+    getBlocksLevels: function () {
+        return [
+            'libs/bem-core/common.blocks',
+            'libs/bem-core/desktop.blocks',
+            'libs/bem-components/common.blocks',
+            'libs/bem-components/desktop.blocks',
+            'desktop.blocks'
+        ];
+    },
+
     bundlesLevelsRegexp: /^.+?\.bundles$/,
 
-    getLevelCachePolicy: function() {
+    getLevelCachePolicy: function () {
         return {
             cache: false,
             except: ['libs/bem-core', 'libs/bem-components']
         }
-    },
-
-    libraries: [
-        'bem-core @ 9df817d2d296e75ee4adebdb117b4bd159ad0109',
-        'bem-components @ 1c153168c56c10c4d74c2ef0a2e21ca39a00fcb5'
-    ]
+    }
 
 });
 
 
 MAKE.decl('BundleNode', {
 
-    getTechs: function() {
+    getTechs: function () {
 
         return [
             'bemjson.js',
@@ -41,12 +39,6 @@ MAKE.decl('BundleNode', {
             'html'
         ];
 
-    },
-
-    'create-browser.js+bemhtml-optimizer-node': function(tech, sourceNode, bundleNode) {
-        sourceNode.getFiles().forEach(function(f) {
-            this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
-        }, this);
     }
 
 });
